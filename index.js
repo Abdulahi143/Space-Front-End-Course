9e0443f4ba5885bbd2f795f68384befe3d387461
unknown <qaskaxaji143@gmail.com>
2024-07-08 20:20:55 +0200

    mostly done

--- index.js
+++ index.js
@@ -7,8 +7,73 @@ navToggle.addEventListener("click", () => {
   navToggle.setAttribute("aria-expanded", !isVisible);
 });
 
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = document.querySelectorAll('[role="tab"]');
 
-// const toggleMenu = (visibility) => nav.setAttribute('data-visible', visibility);
-// const iconVisibility = (visibility) => navToggle.setAttribute('aria-expanded', visibility);
-// navToggle.addEventListener('click', toggleMenu("false"));
-// closeToggle.addEventListener('click', toggleMenu("false"));
+tabList.addEventListener("keydown", changeTab);
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", changeTabPanel);
+});
+
+let tabFocus = 0;
+
+function changeTab(e) {
+  const keydownLeft = 37;
+  const keydownRight = 39;
+
+  if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+    tabs[tabFocus].setAttribute("tabindex", -1);
+  }
+
+  if (e.keyCode === keydownRight) {
+    tabFocus++;
+    if (tabFocus >= tabs.length) {
+      tabFocus = 0;
+    }
+  }
+
+  if (e.keyCode === keydownLeft) {
+    tabFocus--;
+    if (tabFocus < 0) {
+      tabFocus = tabs.length - 1;
+    }
+  }
+
+  tabs[tabFocus].setAttribute("tabindex", 0);
+  tabs[tabFocus].focus();
+}
+
+function changeTabPanel(e) {
+  const targetTab = e.target;
+  const targetPanel = targetTab.getAttribute("aria-controls");
+  const targetImage = targetTab.getAttribute("data-type");
+
+  const tabContainer = targetTab.parentNode;
+  console.log("tab container:", tabContainer);
+  const mainContainer = tabContainer.parentNode;
+
+  console.log("main container:", mainContainer);
+
+  tabContainer
+    .querySelector('[aria-selected="true"]')
+    .setAttribute("aria-selected", false);
+
+  targetTab.setAttribute("aria-selected", true);
+
+  hideCOntent(mainContainer, '[role="tabpanel"]');
+  hideCOntent(mainContainer, "picture");
+
+  showCOntent(mainContainer, `#${targetPanel}`);
+  showCOntent(mainContainer, `#${targetImage}`);
+}
+
+function hideCOntent(parent, content) {
+  parent
+    .querySelectorAll(content)
+    .forEach((item) => item.setAttribute("hidden", true));
+}
+
+function showCOntent(parent, content) {
+  parent.querySelector(content).removeAttribute("hidden");
+}
